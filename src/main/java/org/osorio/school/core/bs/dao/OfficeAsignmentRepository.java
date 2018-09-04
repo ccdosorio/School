16cf24b2ede93273736de25962735f7ce34a4f69
Christian Osorio <ccdosorio11@gmail.com>
2018-09-04 10:56:53 -0600

    Repositorio de Office Assignment

---
+++ src/main/java/org/osorio/school/core/bs/dao/OfficeAsignmentRepository.java
@@ -0,0 +1,8 @@
+package org.osorio.school.core.bs.dao;
+
+import org.osorio.school.core.eis.bo.OfficeAssignment;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface OfficeAsignmentRepository extends PagingAndSortingRepository<OfficeAssignment, Long> {
+
+}
